1671e9c1948c0d066cbf69047a52466730eeb47d
iamakbarsha1 <iamakbarsha1@gmail.com>
2025-02-17 01:04:31 +0530

    learn cargo - package manager

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Bismillah - Prophet Muhammad (SAW)!");
+    println!("Bismillah - Prophet Muhammad Nabi (SAW)!",);
+    println!("Bismillah - Prophet Muhammad Nabi (SAW)!",);
+}
