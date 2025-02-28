18166496a979207013176308f80007662d7cea04
iamakbarsha1 <iamakbarsha1@gmail.com>
2025-03-01 03:01:55 +0530

    start ownership learn - only comments, not code part

---
+++ _4ownership/src/main.rs
@@ -0,0 +1,26 @@
+// Ownership, Borrowing and References
+
+// -----------------------
+// Ownership:
+    // C, C++ -> Memory managament control issue
+    // Garbage collector -> resolved the abv issue, but created a new issue!
+    // New issue with garbage collector: to clean up the memory/data, it will stop/freeze the program
+    //! New Issue resulting in Slow Performance: [stopping/Resuming the program]
+
+// Ownership:
+    // [stopping/Resuming the program]
+    // OWNERSHIP: 
+        // introduced by Rust to solve memory safety issues and high performance at the same time.
+    
+    // What is ownership?
+        // Every value has a single owner [every variable has one value, and it is its sole owner] 
+
+
+
+
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
