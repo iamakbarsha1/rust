fd145fc1f82db28ad198b2d025fc436e59cf3194
iamakbarsha1 <iamakbarsha1@gmail.com>
2025-07-14 11:51:15 +0530

    start learn variables

--- udemy__learn_to_code_with_rust/variables-and-mutability/src/main.rs
+++ udemy__learn_to_code_with_rust/variables-and-mutability/src/main.rs
@@ -1,1 +1,34 @@
-fn main() {}
+/*
+
+Topics Covered in this Section:
+Variables
+
+Interpolation
+
+Positional arguments to println!
+
+Mutability and immutability
+
+Rust error codes
+
+Variable shadowing
+
+Scopes and Blocks
+
+Constants
+
+Type Aliases
+
+Compiler Directives like allow
+
+*/
+
+/*
+*
+*
+*
+*/
+
+fn main() {
+    let apples = 50;
+}
